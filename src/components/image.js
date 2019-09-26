23d152931a74e7b649bd760fba677e6022b2dc84
David Larkin <davidlarkin96@gmail.com>
2019-09-26 15:04:15 +0100

    initial commit

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,22 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+/*import React from 'react'
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
+const Image = ({ image }) => {
+  const element = Object.keys(image).includes("fluid") ? (
+    <picture>
+      <source srcSet={image.fluid.srcSetWebp} type="image/webp" />
+      <source srcSet={image.fluid.srcSet} type="image/png" />
+      <img src={image.fluid.src} title={image.title} alt={image.description} />
+    </picture>
+  ) : (
+    <picture>
+      <source srcSet={image.fixed.srcSetWebp} type="image/webp" />
+      <source srcSet={image.fixed.srcSet} type="image/png" />
+      <img src={image.fixed.src} title={image.title} alt={image.description} />
+    </picture>
+  )
 
-const Image = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
-          }
-        }
-      }
-    }
-  `)
-
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return element
 }
 
 export default Image
+*/
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,10 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
+import homeImage from "../images/laptop-colorful.jpg"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../pages/scss/layout.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -26,22 +20,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+      <div className="container">
+        <img src={homeImage} alt="laptop with colorful background" />
+          <div className="centeredText">
+            <h1>David Larkin</h1>
+            <h3>Software Developer</h3>
+          </div>      
       </div>
-    </>
+      </>
   )
 }
 
