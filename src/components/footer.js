c6f46fcccef133e2bb093206c4c61046c163b5c5
David Larkin <davidlarkin96@gmail.com>
2019-09-26 21:22:34 +0100

    created basic footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../pages/scss/footer.scss"
+
+const Footer = () => (
+  <footer className="footerSection">
+		<div>
+      <ul>
+        <a href="Github">Github</a>
+        <a href="LinkedIn">LinkedIn</a>
+      </ul>
+    </div>
+  </footer> 
+)
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import homeImage from "../images/laptop-colorful.jpg"
 import { useStaticQuery, graphql } from "gatsby"
+import Footer from "./footer"
 
 import Header from "./header"
 import "../pages/scss/layout.scss"
@@ -27,6 +28,8 @@ const Layout = ({ children }) => {
             <h3>Software Developer</h3>
           </div>      
       </div>
+      <Footer></Footer>
+      
       </>
   )
 }
