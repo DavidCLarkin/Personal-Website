28305d40e2ef8e91210ca8efdaa03aadd72be73f
David Larkin <davidlarkin96@gmail.com>
2019-10-24 20:14:53 +0100

    fixing background banner for different viewports, removed padding of project description

--- src/components/banner.js
+++ src/components/banner.js
@@ -4,15 +4,11 @@ import Fade from "react-reveal/Fade"
 
 const Banner= () => (
   <section>
-    <div className="image"/>
-
-    <div className="centered-text">
-      <Fade cascade>
+    <div className="background">
+      <div>
         <h1 className="name">David Larkin</h1>
-      </Fade>
-      <Fade cascade>
         <h3>Software Developer</h3>
-      </Fade>
+      </div>
     </div>
   </section>
 )
