04693cebb07d375e2f59f8151b3a046a0a4773d9
David Larkin <davidlarkin96@gmail.com>
2019-11-11 02:18:25 +0000

    condition checking for undefined in project props

--- src/components/project.js
+++ src/components/project.js
@@ -3,18 +3,19 @@ import styles from "../pages/scss/project.module.scss"
 
 const Project =({ title, description, date, image}) => {
 
-    return (
-      <>
-      <section className={styles.wrapper}>
-        <div className={styles.project}>
-          <h1>{title}</h1>
-          <h4>{date}</h4>
-          <p>{description.description}</p>
-          <img src={image} alt="test"></img>
-        </div>
-      </section>
-      </>
-    );
+  
+  return (
+    <>
+    <section className={styles.wrapper}>
+      <div className={styles.project}>
+        <h1>{title}</h1>
+        <h4>{date}</h4>
+        <p>{description.description}</p>
+        <img src={image} alt="test"></img>
+      </div>
+    </section>
+    </>
+  );
 }
 
 export default Project;
--- src/pages/project-page.js
+++ src/pages/project-page.js
@@ -4,39 +4,34 @@ import Header from "../components/header"
 import Project from "../components/project";
 import Helmet from "react-helmet"
 import "../pages/scss/layout.scss"
-import { throws } from "assert";
 
 const ProjectPage = ({ location }) => {
-
-  /*
-  constructor(props)
-  {
-    super(props);
-    this.state = {
-      title: this.props
-    };
-
-    console.log(this.props);
-  }
-  */
   
-    return (
-      <>
-        <Helmet>
-          <title>David Larkin's Portfolio</title>
-          <meta name="description" content="David Larkin is a software developer with a BSc in Entertainment Systems (Game Development) He has experience in Games, Android, iOS, Desktop and Web Apps"></meta>
-          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        </Helmet>
-        <Header />
-        <Project 
-          title={location.state.title || "nothing"}
-          description={location.state.description || "Nothing"} 
-          date={location.state.date || "Nothing"}
-          image={location.state.image || "Nothing"}
-          />
-        <Footer />
-      </>
-    )
+  const state = {
+    title: "",
+    description: "",
+    date:"",
+    image: ""
+  }
+
+  return (
+    <>
+    {console.log("Title " + state.title)}
+      <Helmet>
+        <title>David Larkin's Portfolio</title>
+        <meta name="description" content="David Larkin is a software developer with a BSc in Entertainment Systems (Game Development) He has experience in Games, Android, iOS, Desktop and Web Apps"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </Helmet>
+      <Header />
+      <Project 
+        title={(location.state.title) ? location.state.title : state.title} 
+        description={(location.state.description) ? location.state.description : state.description} 
+        date={(location.state.date) ? location.state.date : state.date}
+        image={(location.state.image) ? location.state.image : state.image}
+        />
+      <Footer />
+    </>
+  )
 }
 
 export default ProjectPage
