c0a12c78dfc272218463be4f7a31e10753202c16
David Larkin <davidlarkin96@gmail.com>
2019-11-14 19:51:14 +0000

    some small QoL changes and project image expands on hover and increases opacity

--- src/components/header.js
+++ src/components/header.js
@@ -37,22 +37,24 @@ class Header extends React.Component {
   render() {
     return (
     <>
-    <nav className={styles.mobileMenu} id="mobileMenu">
-      <Link to={"/"} className={styles.logo}>DL</Link>
-      <button className={styles.menuBtn} onClick={this.openMenu}>&#9776;</button>
-    </nav>
+      <nav className={styles.mobileMenu} id="mobileMenu">
+        <Link to={"/"} className={styles.logo}>DL</Link>
+        <button className={styles.menuBtn} onClick={this.openMenu}>&#9776;</button>
+      </nav>
 
-    <nav className={styles.navBar} id="nav">
-      <ul className={styles.leftBar}>
-        <li className={styles.logo}>DL</li>
-      </ul>
-      <ul className={styles.rightBar}>
-        <li className={styles.left}><a href="/" onClick={this.closeMenu}>Home</a></li>
-        <li className={styles.left}><a href="/#about" onClick={this.closeMenu}>About</a></li>
-        <li className={styles.left}><a href="/#projects" onClick={this.closeMenu}>Projects</a></li>
-        <li className={styles.left}><a href="/#contact" onClick={this.closeMenu}>Contact</a></li>
-      </ul>
-    </nav>
+      <section className={styles.wrapper}>
+        <nav className={styles.navBar} id="nav">
+          <ul className={styles.leftBar}>
+            <li className={styles.logo}>DL</li>
+          </ul>
+          <ul className={styles.rightBar}>
+            <li className={styles.left}><a href="/" onClick={this.closeMenu}>Home</a></li>
+            <li className={styles.left}><a href="/#about" onClick={this.closeMenu}>About</a></li>
+            <li className={styles.left}><a href="/#projects" onClick={this.closeMenu}>Projects</a></li>
+            <li className={styles.left}><a href="/#contact" onClick={this.closeMenu}>Contact</a></li>
+          </ul>
+        </nav>
+      </section>
     </>
     );
   }
