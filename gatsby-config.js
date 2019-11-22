6a2b8ac138f3656ff562a408de5c797d05746297
David Larkin <davidlarkin96@gmail.com>
2019-11-22 15:18:56 +0000

    changed projects to display in a grid on a larger viewport.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,11 +33,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `personal-site`,
+        short_name: `portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#161616`,
+        theme_color: `#161616`,
         display: `minimal-ui`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
